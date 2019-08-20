579250d52eb11e5c1503de06de965968b9d67cb4
XeoNovaDan <toghill7252010@hotmail.co.uk>
2019-08-20 01:38:31 +0100

    Backstory stuff

---
+++ Source/VanillaFactionsExpandedMedieval/DefModExtensions/ResearchProjectDefExtension.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using Verse;
+using RimWorld;
+
+namespace VFEMedieval
+{
+
+    public class ResearchProjectDefExtension : DefModExtension
+    {
+
+        public static readonly ResearchProjectDefExtension defaultValues = new ResearchProjectDefExtension();
+
+        public List<ResearchProjectTagDef> greylistedTags;
+
+    }
+
+}
